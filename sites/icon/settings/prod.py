f071d3e6de9eb4e49d1c199a90712fed12084cde
pavlo.bogomolenko <pavlo.bogomolenko@aoemedia.de>
2014-03-16 15:53:16 +0100

    s3 storage compressor settings

--- sites/icon/settings/prod.py
+++ sites/icon/settings/prod.py
@@ -15,9 +15,11 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 # LESS/CSS/statics
 # ================
 #USE_LESS = True
-#COMPRESS_ENABLED = True
+COMPRESS_ENABLED = True
 
 STATIC_URL = 'https://icon-oscar.s3.amazonaws.com/'
+COMPRESS_URL = STATIC_URL
+COMPRESS_ROOT = STATIC_ROOT
 
 #aws configuration
 if 'S3_KEY' in os.environ and 'S3_SECRET' in os.environ: 
@@ -25,16 +27,21 @@ if 'S3_KEY' in os.environ and 'S3_SECRET' in os.environ:
 	AWS_SECRET_ACCESS_KEY = os.environ['S3_SECRET']
 
 # aws S3 bucket configuration
-#DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_STORAGE_BUCKET_NAME = 'icon-oscar'
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#COMPRESS_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'sites.icon.apps.oscartweaks.storage.CachedS3BotoStorage'
+COMPRESS_STORAGE = 'sites.icon.apps.oscartweaks.storage.CachedS3BotoStorage'
 AWS_S3_FILE_OVERWRITE = False
+AWS_PRELOAD_METADATA = True
 
 # see http://developer.yahoo.com/performance/rules.html#expires
-AWS_HEADERS = {
-	'Expires': 'Thu, 15 Apr 2010 20:00:00 GMT',
-	'Cache-Control': 'max-age=86400',
-}
+# AWS_HEADERS = {
+# 	'Expires': 'Thu, 15 Apr 2010 20:00:00 GMT',
+# 	'Cache-Control': 'max-age=86400',
+# }
 
 INSTALLED_APPS = INSTALLED_APPS + ['sites.icon.apps.oscartweaks']
+
+DEBUG = True
